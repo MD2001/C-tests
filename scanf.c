3639e1044c90fc18314b20e6108b2e2880fef888
MD2001 <72444541+MD2001@users.noreply.github.com>
2022-07-20 09:13:50 +0200

    Add files via upload
    this is function in stdio.h library used to take data from user

---
+++ scanf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(void)
+{
+	int x;
+	//ask user to enter value
+	printf("Enter num :");
+	//take value and assigne it to variable in decimal
+	scanf("%d",&x);
+	return 0;
+}
