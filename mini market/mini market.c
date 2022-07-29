8b2cfe4efbaf801c57c505ebb1dac66d565c85af
MD2001 <72444541+MD2001@users.noreply.github.com>
2022-07-29 21:37:03 +0200

    Update mini market.c

--- mini market/mini market.c
+++ mini market/mini market.c
@@ -7,7 +7,7 @@ int main (void)
 	char names[20][12]={"","","","","","","","","","apple","banana","orange"};
 	double sales=0.0;
 	int quantity=0;
-	int	part,chose;
+	int	part,chose,tdelvary;
 	char flag='c';
 	printf("==============welcome to my Market================\n");
 	while(flag != 'n')
@@ -19,15 +19,58 @@ int main (void)
 		switch(part)
 		{
 			case 1 :
+			printf("================bakery part================\n");
+			printf("to get product enter it num: {1-%s:%d$ ,2-%s:%d$ ,3-%s:%d$ }\nnum: ",
+			names[0],prices[0],names[1],prices[1],names[2],prices[2]);
+			scanf("%d",&chose);
+			printf("quantity: ");
+			scanf("%d",&quantity);
+			
+			switch(chose)
+			{
+				case 1 : products[0]+=quantity; break;
+				case 2 : products[1]+=quantity; break;
+				case 3 : products[2]+=quantity; break;
+				default: printf("you Enter unvalide num!!!! please try agine\n"); break;
+			}
 			break;
 			case 2 :
+			printf("================Dairy products part================\n");
+			printf("to get product enter it num: {1-%s:%d$ ,2-%s:%d$ ,3-%s:%d$ }\nnum: ",
+			names[3],prices[3],names[4],prices[4],names[5],prices[5]);
+			scanf("%d",&chose);
+			printf("quantity: ");
+			scanf("%d",&quantity);
+			
+			switch(chose)
+			{
+				case 1 : products[3]+=quantity; break;
+				case 2 : products[4]+=quantity; break;
+				case 3 : products[5]+=quantity; break;
+				default: printf("you Enter unvalide num!!!! please try agine\n"); break;
+			}
 			break;
 			case 3:
+			printf("================vegetables part================\n");
+			printf("to get product enter it num: {1-%s:%d$ ,2-%s:%d$ ,3-%s:%d$ }\nnum: ",
+			names[6],prices[6],names[7],prices[7],names[8],prices[8]);
+			scanf("%d",&chose);
+			printf("quantity: ");
+			scanf("%d",&quantity);
+			
+			switch(chose)
+			{
+				case 1 : products[6]+=quantity; break;
+				case 2 : products[7]+=quantity; break;
+				case 3 : products[8]+=quantity; break;
+				default: printf("you Enter unvalide num!!!! please try agine\n"); break;
+			}
+			
 			break;
 			case 4 :
 			printf("================fruits part================\n");
-			printf("to get product enter it num: {1-apple:%d$ ,2-banana:%d$ ,3-orange:%d$ }\nnum: ",
-			prices[9],prices[10],prices[11]);
+			printf("to get product enter it num: {1-%s:%d$ ,2-%s:%d$ ,3-%s:%d$ }\nnum: ",
+			names[9],prices[9],names[10],prices[10],names[11],prices[11]);
 			scanf("%d",&chose);
 			printf("quantity: ");
 			scanf("%d",&quantity);
@@ -56,11 +99,11 @@ int main (void)
 			}				
 		}
 		printf("your proudct num is : %d\n",pnum);
-		printf("your cost is : %0.2f$\n",sales);
 		if(pnum>=5)
 		{
 			sales*=0.2;
 		}
+		printf("your cost is : %0.2f$\n",sales);
 		printf("================================================\n");
 		printf("for end selling please Enter n else Enter any key\n");
 		scanf(" %c",&flag);
@@ -69,10 +112,45 @@ int main (void)
 	}
 	if(pnum<2)
 	{
-		printf("you must have more than 1 item to get order");
+		printf("you must have more than 1 item to get order\n");
 	}
 	else
 	{
+		printf("delevary:\n");
+		printf("1-night\n2-morning\n");
+		printf("num: ");
+		scanf("%d",&tdelvary);
+		if(sales>10)
+		{
+			sales+=3;
+		}
+		else
+		{
+			sales+=2;
+		}
+		if(tdelvary==1)
+		{
+			sales+=5;
+		}
+		printf("==================your fee=======================\n");
+		sales=0;
+		pnum=0;
+		for(int i=0;i<12;i++)
+		{ 
+			if(products[i]!=0)
+			{ pnum++;
+				sales+=prices[i]*products[i];
+				printf("%s : %d\n",names[i],products[i]);
+			}				
+		}
+		printf("your proudct num is : %d\n",pnum);
+		if(pnum>=5)
+		{
+			sales*=0.2;
+		}
+		printf("your cost is : %0.2f$\n",sales);
+		printf("================================================\n");
+		printf("thank you for shoping!");
 		
 	}
 	
