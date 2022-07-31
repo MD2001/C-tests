01cf505fb1c9e177e3dcf8c5962a4b2f24445590
MD2001 <72444541+MD2001@users.noreply.github.com>
2022-07-31 13:40:23 +0200

    Add files via upload

---
+++ ITI tranning/lab3.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void main(void)
+
+{
+	int num1=0;
+	int num2=0;
+	printf("please Enter the num1 : ");
+	scanf("%d",&num1);
+	printf("please Enter the num2 : ");
+	scanf("%d",&num2);
+	printf("the %d + %d = %d \n",num1,num2,(num1+num2));
+	printf("the %d * %d = %d \n",num1,num2,(num1*num2));
+}
