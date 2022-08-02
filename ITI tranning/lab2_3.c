9bb3b1c18478852645669adcabbb5faa4315517a
MD2001 <72444541+MD2001@users.noreply.github.com>
2022-08-02 11:03:14 +0200

    Add files via upload

---
+++ ITI tranning/lab2_3.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+void max(int x,int y);
+int main()
+{
+	int num1,num2;
+	printf("Enter num1 : ");
+	scanf("%d",&num1);
+	printf("Enter num2 : ");
+	scanf("%d",&num2);
+	max(num1,num2);
+}
+void max(int x,int y)
+{
+	if(x>y)
+	{
+		printf("max is %d",x);
+	}
+	else{
+		printf("max is %d",y);
+	}
+}
