e33514ecffe65abb7415a5b8c15e6bc7950d7ad5
Mohamed Diaa <72444541+MD2001@users.noreply.github.com>
2022-08-11 10:49:39 +0200

    Add files via upload

---
+++ ATMEGA32/LIB/STD_TYPES.h
@@ -0,0 +1,20 @@
+#ifndef STD_TYPES_H_
+#define STD_TYPES_H_
+
+/*unsigned Integer data types*/
+typedef unsigned char         u8;
+typedef unsigned short int    u16;
+typedef unsigned long int     u32;
+typedef unsigned long  long   u64;
+/*signed Integer data types*/				  
+typedef signed char          s8;
+typedef signed short  int    s16;
+typedef signed long int      s32;
+typedef signed long  long    s64;
+/*Real data types*/
+typedef float            f32;
+typedef double           f64;
+typedef long double      f96;
+					     
+#endif
+
