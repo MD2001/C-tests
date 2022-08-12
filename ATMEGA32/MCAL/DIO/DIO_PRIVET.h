1b4faa0d50d8c367aaf77e4ef33b41ac5f868ad8
Mohamed Diaa <72444541+MD2001@users.noreply.github.com>
2022-08-12 16:16:35 +0200

    Update DIO_PRIVET.h

--- ATMEGA32/MCAL/DIO/DIO_PRIVET.h
+++ ATMEGA32/MCAL/DIO/DIO_PRIVET.h
@@ -2,7 +2,20 @@
 #define DIO_PRIVET_H
 
 
-#define PORTA_RG      *((u8 * )0x3A)
+#define PORTA_RG      *((u8 * )0x3B)
+#define PORTB_RG      *((u8 * )0x38)
+#define PORTC_RG      *((u8 * )0x35)
+#define PORTD_RG      *((u8 * )0x32)
+
+#define DDRA_RG      *((u8 * )0x3A)
+#define DDRB_RG      *((u8 * )0x37)
+#define DDRC_RG      *((u8 * )0x34)
+#define DDRD_RG      *((u8 * )0x31)
+
+#define PINA_RG      *((u8 * )0x39)
+#define PINB_RG      *((u8 * )0x36)
+#define PINC_RG      *((u8 * )0x33)
+#define PIND_RG      *((u8 * )0x30)
 
 
 
