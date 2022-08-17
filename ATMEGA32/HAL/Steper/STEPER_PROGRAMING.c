a69878ad3764873cfc1385094d3cbc6f559034e3
Mohamed Diaa <72444541+MD2001@users.noreply.github.com>
2022-08-17 11:44:57 +0200

    Add files via upload

---
+++ ATMEGA32/HAL/Steper/STEPER_PROGRAMING.c
@@ -0,0 +1,103 @@
+/***********************************************************************************************************/
+/***************************** Author : Mohamed diaa********************************************************/
+/***************************** Layer : HAL *****************************************************************/
+/***************************** SWC : STERPER ***************************************************************/
+/***************************** Version : 1.0 ***************************************************************/
+#include "avr/delay.h"
+#include "../../LIB/STD_TYPES.h"
+#include "../../LIB/BIT_MATH.h"
+#include "../../MCAL/DIO/DIO_INTERFACE.h"
+#include "STEPER_CONFIG.h"
+#include "STEPER_INTERFACE.h"
+#include "STEPER_PRIVET.h"
+
+
+
+void STEPER_voidInit(void)
+{
+
+	DIO_SetPinDirection(BLUE_WIRE,DIO_OUTPUT);
+	DIO_SetPinDirection(ORANGE_WIRE,DIO_OUTPUT);
+	DIO_SetPinDirection(YELLOW_WIRE,DIO_OUTPUT);
+	DIO_SetPinDirection(RED_WIRE,DIO_OUTPUT);
+
+}
+void STEPER_voidFullStep(void)
+{
+	DIO_SetPinValue(BLUE_WIRE,DIO_HIGH);
+	DIO_SetPinValue(ORANGE_WIRE,DIO_LOW);
+	DIO_SetPinValue(YELLOW_WIRE,DIO_LOW);
+	DIO_SetPinValue(RED_WIRE,DIO_LOW);
+	_delay_ms(1500);
+	DIO_SetPinValue(BLUE_WIRE,DIO_LOW);
+	DIO_SetPinValue(ORANGE_WIRE,DIO_HIGH);
+	DIO_SetPinValue(YELLOW_WIRE,DIO_LOW);
+	DIO_SetPinValue(RED_WIRE,DIO_LOW);
+	_delay_ms(1500);
+	DIO_SetPinValue(BLUE_WIRE,DIO_LOW);
+	DIO_SetPinValue(ORANGE_WIRE,DIO_LOW);
+	DIO_SetPinValue(YELLOW_WIRE,DIO_HIGH);
+	DIO_SetPinValue(RED_WIRE,DIO_LOW);
+	_delay_ms(1500);
+	DIO_SetPinValue(BLUE_WIRE,DIO_LOW);
+	DIO_SetPinValue(ORANGE_WIRE,DIO_LOW);
+	DIO_SetPinValue(YELLOW_WIRE,DIO_LOW);
+	DIO_SetPinValue(RED_WIRE,DIO_HIGH);
+	_delay_ms(1500);
+	DIO_SetPinValue(BLUE_WIRE,DIO_HIGH);
+	DIO_SetPinValue(ORANGE_WIRE,DIO_LOW);
+	DIO_SetPinValue(YELLOW_WIRE,DIO_LOW);
+	DIO_SetPinValue(RED_WIRE,DIO_LOW);
+	_delay_ms(1500);
+}
+
+void STEPER_voidHalfStep(void)
+{
+	DIO_SetPinValue(BLUE_WIRE,DIO_HIGH);
+	DIO_SetPinValue(ORANGE_WIRE,DIO_LOW);
+	DIO_SetPinValue(YELLOW_WIRE,DIO_LOW);
+	DIO_SetPinValue(RED_WIRE,DIO_HIGH);
+	_delay_ms(1500);
+	DIO_SetPinValue(BLUE_WIRE,DIO_HIGH);
+	DIO_SetPinValue(ORANGE_WIRE,DIO_LOW);
+	DIO_SetPinValue(YELLOW_WIRE,DIO_LOW);
+	DIO_SetPinValue(RED_WIRE,DIO_LOW);
+	_delay_ms(1500);
+	DIO_SetPinValue(BLUE_WIRE,DIO_HIGH);
+	DIO_SetPinValue(ORANGE_WIRE,DIO_HIGH);
+	DIO_SetPinValue(YELLOW_WIRE,DIO_LOW);
+	DIO_SetPinValue(RED_WIRE,DIO_LOW);
+	_delay_ms(1500);
+	DIO_SetPinValue(BLUE_WIRE,DIO_LOW);
+	DIO_SetPinValue(ORANGE_WIRE,DIO_HIGH);
+	DIO_SetPinValue(YELLOW_WIRE,DIO_LOW);
+	DIO_SetPinValue(RED_WIRE,DIO_LOW);
+	_delay_ms(1500);
+	DIO_SetPinValue(BLUE_WIRE,DIO_LOW);
+	DIO_SetPinValue(ORANGE_WIRE,DIO_HIGH);
+	DIO_SetPinValue(YELLOW_WIRE,DIO_HIGH);
+	DIO_SetPinValue(RED_WIRE,DIO_LOW);
+	_delay_ms(1500);
+	DIO_SetPinValue(BLUE_WIRE,DIO_LOW);
+	DIO_SetPinValue(ORANGE_WIRE,DIO_LOW);
+	DIO_SetPinValue(YELLOW_WIRE,DIO_HIGH);
+	DIO_SetPinValue(RED_WIRE,DIO_LOW);
+	_delay_ms(1500);
+	DIO_SetPinValue(BLUE_WIRE,DIO_LOW);
+	DIO_SetPinValue(ORANGE_WIRE,DIO_LOW);
+	DIO_SetPinValue(YELLOW_WIRE,DIO_HIGH);
+	DIO_SetPinValue(RED_WIRE,DIO_HIGH);
+	_delay_ms(1500);
+	DIO_SetPinValue(BLUE_WIRE,DIO_LOW);
+	DIO_SetPinValue(ORANGE_WIRE,DIO_LOW);
+	DIO_SetPinValue(YELLOW_WIRE,DIO_LOW);
+	DIO_SetPinValue(RED_WIRE,DIO_HIGH);
+	_delay_ms(1500);
+	DIO_SetPinValue(BLUE_WIRE,DIO_HIGH);
+	DIO_SetPinValue(ORANGE_WIRE,DIO_LOW);
+	DIO_SetPinValue(YELLOW_WIRE,DIO_LOW);
+	DIO_SetPinValue(RED_WIRE,DIO_HIGH);
+	_delay_ms(1500);
+
+
+}
